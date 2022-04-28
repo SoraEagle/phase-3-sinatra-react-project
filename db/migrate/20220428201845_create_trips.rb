df1824b2b5c4948e2a1bff52d54a836b27585288
Matthew Phillips <phillips.matthew829@gmail.com>
2022-04-28 15:38:56 -0500

    add create_table to migrations files

--- db/migrate/20220428201845_create_trips.rb
+++ db/migrate/20220428201845_create_trips.rb
@@ -1,4 +1,7 @@
 class CreateTrips < ActiveRecord::Migration[6.1]
   def change
+    create_table :trips do |t|
+      t.string :name
+      t.timestamps
   end
 end
--- db/migrate/20220428202129_create_items.rb
+++ db/migrate/20220428202129_create_items.rb
@@ -1,4 +1,11 @@
 class CreateItems < ActiveRecord::Migration[6.1]
   def change
+    create_table :items do |t|
+      t.string :name
+      t.integer :trip_id # Foreign Key
+      t.timestamps
+    end
+    # add trip_id as a foreign key
+    add_foreign_key :items, :trips
   end
 end
