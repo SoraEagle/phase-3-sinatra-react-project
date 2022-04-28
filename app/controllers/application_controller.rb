71598486ae7c8dc2a06ad81a066e1c791d06e44c
Matthew Phillips <phillips.matthew829@gmail.com>
2022-04-28 16:54:05 -0500

    add get and delete routers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,5 +7,21 @@ class ApplicationController < Sinatra::Base
   end
 
   # Add get, post, patch, and delete routes here:
+  get '/trips/:id' do # List the trip and its items
+    trip = Trip.find(params[:id])
+    items = trip.items.all # All of that trip's items
+    trip.to_json
+    items.to_json
+  end
 
+  # post '/trips' do
+  #   trip = Trip.create()
+  #   trip.to_json
+  # end
+
+  delete '/item/:id' do # Deletes the corresponding item
+    item = Item.find(params[:id])
+    item.destroy
+    item.to_json
+  end
 end
