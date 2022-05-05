704a978358d1529d15d37ff71fb5b5c01b647656
Matthew Phillips <phillips.matthew829@gmail.com>
2022-05-05 15:49:46 -0500

    Deleting a trip deletes its items

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,24 +13,24 @@ class ApplicationController < Sinatra::Base
     trips.to_json
   end
 
-  trip_list = get '/trips/:id' do # List the specified trip and its items
+  get '/trips/:id' do # List the specified trip and its items
     trips = Trip.find(params[:id])
     trips.to_json(include: :items)
   end
 
-  all_items = get '/items' do # List all items
+  get '/items' do # List all items
     items = Item.all
     items.to_json
   end
 
-  create_trip = post '/trips' do # Create a trip
+  post '/trips' do # Create a trip
     trip = Trip.create(
       name: params[:name]
     ) # Add named parameters for text fields
     trip.to_json
   end
 
-  create_item = post '/items' do # Create a item
+  post '/items' do # Create a item
     item = Item.create(
       name: params[:name],
       trip_id: params[:trip_id]
@@ -38,13 +38,13 @@ class ApplicationController < Sinatra::Base
     item.to_json
   end
 
-  update_item = patch '/items/:id' do # Update specific item's info
+  patch '/items/:id' do # Update specific item's info
     items = Item.find(param[:id])
     items.update(name: params[:name])
     items.to_json
   end
 
-  delete_item = delete '/items/:id' do # Deletes the corresponding item
+  delete '/items/:id' do # Deletes the corresponding item
     item = Item.find(params[:id])
     item.destroy
     item.to_json
